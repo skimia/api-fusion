49a791d3fec8eadee25e6adafcbf8d3c324c3c01
Travis <travis@travis-ci.org>
2015-11-28 12:26:04 +0100

    add dependencies loading

--- src/ApiFusionServiceProvider.php
+++ src/ApiFusionServiceProvider.php
@@ -3,6 +3,16 @@
 
 
 use Illuminate\Support\ServiceProvider;
+use Dingo\Api\Provider\LaravelServiceProvider as DingoApiServiceProvider;
+use KennedyTedesco\Validation\ValidationServiceProvider;
+use Cartalyst\Sentinel\Laravel\SentinelServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+use Dingo\Api\Facade\API;
+use Dingo\Api\Facade\Route;
+use Cartalyst\Sentinel\Laravel\Facades\Activation;
+use Cartalyst\Sentinel\Laravel\Facades\Reminder;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 
 class ApiFusionServiceProvider extends ServiceProvider
 {
@@ -11,7 +21,19 @@ class ApiFusionServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // TODO: Implement register() method.
+        $this->app->register(DingoApiServiceProvider::class);
+        $this->app->register(SentinelServiceProvider::class);
+        $this->app->register(ValidationServiceProvider::class);
+
+        $loader = AliasLoader::getInstance();
+
+        $loader->alias('API', API::class);
+        $loader->alias('APIRoute', Route::class);
+
+        $loader->alias('Activation', Activation::class);
+        $loader->alias('Reminder', Reminder::class);
+        $loader->alias('Sentinel', Sentinel::class);
+        
     }
 
 }
